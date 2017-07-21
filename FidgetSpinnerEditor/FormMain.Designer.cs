7d47188103a34f31ffa20ae863d14780b6c0a522
Erwin Ried <erwinried@gmail.com>
2017-07-21 22:33:33 +0200

    First commit

---
+++ FidgetSpinnerEditor/FormMain.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace FidgetSpinnerEditor
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxEditor = new System.Windows.Forms.PictureBox();
+            this.menuStripMain = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileDialogBin = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialogBin = new System.Windows.Forms.OpenFileDialog();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutProgramToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxEditor)).BeginInit();
+            this.menuStripMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBoxEditor
+            // 
+            this.pictureBoxEditor.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxEditor.Location = new System.Drawing.Point(12, 27);
+            this.pictureBoxEditor.Name = "pictureBoxEditor";
+            this.pictureBoxEditor.Size = new System.Drawing.Size(640, 484);
+            this.pictureBoxEditor.TabIndex = 0;
+            this.pictureBoxEditor.TabStop = false;
+            this.pictureBoxEditor.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBoxEditor_Paint);
+            this.pictureBoxEditor.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBoxEditor_MouseMove);
+            this.pictureBoxEditor.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBoxEditor_MouseMove);
+            // 
+            // menuStripMain
+            // 
+            this.menuStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStripMain.Location = new System.Drawing.Point(0, 0);
+            this.menuStripMain.Name = "menuStripMain";
+            this.menuStripMain.Size = new System.Drawing.Size(664, 24);
+            this.menuStripMain.TabIndex = 1;
+            this.menuStripMain.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.quitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "&Open...";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveAsToolStripMenuItem.Text = "&Save as...";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.quitToolStripMenuItem.Text = "&Exit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // openFileDialogBin
+            // 
+            this.openFileDialogBin.Filter = "Bin file (*.bin)|*.bin";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutProgramToolStripMenuItem});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "&About";
+            // 
+            // aboutProgramToolStripMenuItem
+            // 
+            this.aboutProgramToolStripMenuItem.Name = "aboutProgramToolStripMenuItem";
+            this.aboutProgramToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.aboutProgramToolStripMenuItem.Text = "&About program";
+            this.aboutProgramToolStripMenuItem.Click += new System.EventHandler(this.aboutProgramToolStripMenuItem_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(664, 523);
+            this.Controls.Add(this.pictureBoxEditor);
+            this.Controls.Add(this.menuStripMain);
+            this.MainMenuStrip = this.menuStripMain;
+            this.Name = "FormMain";
+            this.Text = "Fidget Spinner Editor";
+            this.Resize += new System.EventHandler(this.FormMain_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxEditor)).EndInit();
+            this.menuStripMain.ResumeLayout(false);
+            this.menuStripMain.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxEditor;
+        private System.Windows.Forms.MenuStrip menuStripMain;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialogBin;
+        private System.Windows.Forms.OpenFileDialog openFileDialogBin;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutProgramToolStripMenuItem;
+    }
+}
+
---
+++ FidgetSpinnerEditor/FormMain.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FidgetSpinnerEditor
+{
+    public partial class FormMain : Form
+    {
+        private const int LedRows = 16, LedColumns = 120;
+        private int LedSize;
+        private BitArray _bits = new BitArray(LedRows * LedColumns);
+        private Rectangle drawingArea;
+        private PointF center;
+        private RectangleF innerDrawingArea;
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBoxEditor_Paint(object sender, PaintEventArgs e)
+        {
+            var m = Math.Min(pictureBoxEditor.DisplayRectangle.Height, pictureBoxEditor.DisplayRectangle.Width)-2;
+            LedSize = m / 80;
+            drawingArea = new Rectangle(0, 0, m, m);
+            center = new PointF(drawingArea.Left + drawingArea.Width / 2, drawingArea.Top + drawingArea.Height / 2);
+            var mInner = m / 3;
+            innerDrawingArea = new RectangleF(center.X - mInner / 2, center.Y - mInner / 2, mInner, mInner);
+
+            e.Graphics.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighSpeed;
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            e.Graphics.FillRectangle(Brushes.White, pictureBoxEditor.DisplayRectangle);
+            e.Graphics.FillEllipse(Brushes.Yellow, drawingArea);
+            e.Graphics.DrawEllipse(Pens.Black, drawingArea);
+            
+            e.Graphics.FillEllipse(Brushes.White, innerDrawingArea);
+            e.Graphics.DrawEllipse(Pens.Black, innerDrawingArea);
+
+            for (int i = 0; i < LedRows; i++)
+            {
+                var p = new PointF(center.X, ((drawingArea.Height - innerDrawingArea.Height) / 2) / (LedRows + 1) * (1+i));
+
+                for (int j = 0; j < LedColumns; j++)
+                {
+                    e.Graphics.FillEllipse(GetBit(i+j*LedRows)?Brushes.Blue:Brushes.LightYellow, p.X - LedSize/2, p.Y - LedSize/2, LedSize, LedSize);
+                    p = RotatePoint(p, center, 360 / LedColumns);
+                }
+            }
+        }
+
+        private void LoadBin(string path)
+        {
+            _bits = new BitArray((File.ReadAllBytes(path)).SelectMany(GetBits).ToArray());
+        }
+
+        private static IEnumerable<bool> GetBits(byte b)
+        {
+            for (var i = 0; i < 8; i++)
+            {
+                yield return (b & 0x80) != 0;
+                b *= 2;
+            }
+        }
+
+        private bool GetBit(int bit)
+        {
+            if (_bits != null && _bits.Length > bit)
+                return _bits[bit];
+            return false;
+        }
+
+        private static PointF RotatePoint(PointF pointToRotate, PointF centerPoint, double angleInDegrees)
+        {
+            var angleInRadians = angleInDegrees * (Math.PI / 180);
+            var cosTheta = Math.Cos(angleInRadians);
+            var sinTheta = Math.Sin(angleInRadians);
+
+            return new PointF
+            {
+                X =
+                    (float) (cosTheta * (pointToRotate.X - centerPoint.X) -
+                             sinTheta * (pointToRotate.Y - centerPoint.Y) + centerPoint.X),
+                Y =
+                    (float) (sinTheta * (pointToRotate.X - centerPoint.X) +
+                             cosTheta * (pointToRotate.Y - centerPoint.Y) + centerPoint.Y)
+            };
+        }
+
+        private void FormMain_Resize(object sender, EventArgs e)
+        {
+            pictureBoxEditor.Invalidate();
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialogBin.ShowDialog() == DialogResult.OK)
+            {
+                LoadBin(openFileDialogBin.FileName);
+                pictureBoxEditor.Invalidate();
+            }
+        }
+
+        private void pictureBoxEditor_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(e.Button != MouseButtons.None)
+            {
+                var p = GetBitByPosition(e.Location);
+
+                if (p == -1)
+                    return;
+
+                var v = e.Button == MouseButtons.Left;
+
+                if (_bits[p] != v)
+                {
+                    _bits[p] = v;
+                    pictureBoxEditor.Invalidate();
+                }
+            }
+        }
+
+        private void aboutProgramToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Programmed by Erwin Ried", "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private int GetBitByPosition(Point point)
+        {
+            if (!drawingArea.IsEmpty && !innerDrawingArea.IsEmpty && !center.IsEmpty)
+                for (var i = 0; i < LedRows; i++)
+                {
+                    var p = new PointF(center.X,
+                        ((drawingArea.Height - innerDrawingArea.Height) / 2) / (LedRows + 1) * (1 + i));
+
+                    for (var j = 0; j < LedColumns; j++)
+                    {
+                        if (new RectangleF(p.X - LedSize / 2, p.Y - LedSize / 2, LedSize, LedSize).Contains(point))
+                            return (i + j * LedRows);
+                        p = RotatePoint(p, center, 360 / LedColumns);
+                    }
+                }
+
+            return -1;
+        }
+    }
+}
